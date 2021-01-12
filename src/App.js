2890d4c5b0b49487a858fe95da95f519441a04b6
Paolo Moroni <paolo@codedby.pm>
2021-01-12 22:43:12 +0100

    feat: setup Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.scss";
+import ReactGA from 'react-ga';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-89931383-1');
+    ReactGA.pageview(window.location.pathname);
+  })
+
   return (
     <BrowserRouter>
       <Switch>
