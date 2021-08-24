6bf8a0cc63d40e694e5c2ee9260b18ab16a159df
Paolo Moroni <paolo@codedby.pm>
2021-08-24 21:46:28 +0200

    Revert "refactor: move theming in Home.js"
    This reverts commit 013b44d0b87c353548f05c7648515edf1a816b44.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,50 +11,37 @@ import Pricing from '../components/sections/Pricing';
 import Services from '../components/sections/Services';
 import Testimonials from '../components/sections/Testimonials';
 import Works from '../components/sections/Works';
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from '../components/elements/GlobalStyle';
-import { light, dark } from '../components/elements/Themes';
-import Toggle from '../components/elements/Toggle';
 
 function Home() {
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light');
-  };
-
-  document.body.classList.add('dark');
+  document.body.classList.add("dark");
   return (
     <>
-      <ThemeProvider theme={theme === 'light' ? light : dark}>
-        <GlobalStyle />
-        <main className='content'>
-          <Toggle></Toggle>
-          <Element name='section-home'>
-            <ReactCursorPosition>
-              <Herosection />
-            </ReactCursorPosition>
-          </Element>
-          <Element name='section-about'>
-            <About />
-          </Element>
-          {/* <Element name="section-services">
+      <main className="content">
+        <Element name="section-home">
+          <ReactCursorPosition>
+            <Herosection />
+          </ReactCursorPosition>
+        </Element>
+        <Element name="section-about">
+          <About />
+        </Element>
+        {/* <Element name="section-services">
           <Services />
         </Element> */}
-          <Element name='section-experiences'>
-            <Experiences />
-          </Element>
-          <Element name='section-testimoninal'>
-            <Testimonials />
-          </Element>
-          <Element name='section-brandlogos'>
-            <Brandlogos />
-          </Element>
-          <Element name='section-contact'>
-            <Contact />
-          </Element>
-          <div className='spacer' data-height='96'></div>
-        </main>
-      </ThemeProvider>
+        <Element name="section-experiences">
+          <Experiences />
+        </Element>
+        <Element name="section-testimoninal">
+          <Testimonials />
+        </Element>
+        <Element name="section-brandlogos">
+          <Brandlogos />
+        </Element>
+        <Element name="section-contact">
+          <Contact />
+        </Element>
+        <div className="spacer" data-height="96"></div>
+      </main>
     </>
   );
 }
